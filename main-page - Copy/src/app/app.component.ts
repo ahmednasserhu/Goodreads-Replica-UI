f788296ad15eeae9767850a38f530d94440464a8
shimaaFathi <shimaafathi126@gmail.com>
2024-02-19 01:24:01 +0200

    main3

--- main-page - Copy/src/app/app.component.ts
+++ main-page - Copy/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './register/register.component'
 import { LoginComponent } from './login/login.component';
+import { FormErrorMsgComponent } from './form-error-msg/form-error-msg.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, RegisterComponent, LoginComponent],
+  imports: [RouterOutlet, HeaderComponent, RegisterComponent, LoginComponent,FormErrorMsgComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- main-page - Copy/src/app/register/register.component.ts
+++ main-page - Copy/src/app/register/register.component.ts
@@ -1,81 +1,40 @@
 import { Component } from '@angular/core';
-import {FormControl, Validators, FormsModule, ReactiveFormsModule,FormGroup, FormBuilder} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { CommonModule } from '@angular/common';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { FormErrorMsgComponent } from '../form-error-msg/form-error-msg.component';
+import { passwordMatch } from '../valid/valid.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [RouterLink, ReactiveFormsModule, FormErrorMsgComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  registrationForm: FormGroup;
+  registerForm: FormGroup;
+  selectedFile: File | undefined;
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
+      password: ['', [Validators.required, Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,32}$")]],
+      confirmPassword: ['', [Validators.required]]
+    },
+      {
+        validators: passwordMatch
+      }
+    )
+  }
 
-  constructor(private formBuilder: FormBuilder) {
-    this.registrationForm = this.formBuilder.group({
-    });
-  }
-  firstName = new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]);
-  lastName = new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/)]);
-  confirmPassword = new FormControl('', [Validators.required]);
-  
-  getErrorMessage(control: FormControl, fieldName: string) {
-    console.log('Control value:', control.value);
-    console.log('Password value:', this.password.value);
-  
-    if (control.invalid && (control.dirty || control.touched)) {
-      if (control.hasError('required')) {
-        return `You must enter ${fieldName}`;
-      }
-  
-      if (fieldName === 'email' && control.hasError('email')) {
-        return 'Not a valid email';
-      }
-  
-      if (fieldName === 'email' && control.value !== null) {
-        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        if (!emailRegex.test(control.value)) {
-          return 'Please enter a valid email address';
-        }
-      }
-  
-      if (fieldName === 'firstName' && control.hasError('pattern')) {
-        return 'First name must contain only letters';
-      }
-  
-      if (fieldName === 'lastName' && control.hasError('pattern')) {
-        return 'Last name must contain only letters';
-      }
-  
-      if (fieldName === 'password' && control.hasError('minlength')) {
-        return 'Password must be at least 8 characters long';
-      }
-  
-      if (fieldName === 'password' && control.hasError('pattern')) {
-        return 'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character';
-      }
-  
-      if (fieldName === 'confirmPassword' && this.password.value !== control.value) {
-        return 'Passwords do not match';
-      }
-    }
-  
-    return '';
-  }
-  
   onSubmit() {
-    if (this.registrationForm.valid) {
-     console.log('Form submitted successfully!');
-   } else {
-      console.error('Form has validation errors');
+    if (this.registerForm.valid) {
+      this.router.navigate(["/login"]);
+      window.alert(`User [${this.registerForm.controls['username'].value}] Successfully Registered`);
     }
   }
-  
-  
-  
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
 }
