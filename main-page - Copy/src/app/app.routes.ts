eb166074d619ba70a9259d60f85b64dd4b8d831a
shimaaFathi <shimaafathi126@gmail.com>
2024-02-23 14:42:03 +0200

    ..

--- main-page - Copy/src/app/app.routes.ts
+++ main-page - Copy/src/app/app.routes.ts
@@ -7,6 +7,9 @@ import { AllTableComponent } from './all-table/all-table.component';
 import { ReadTableComponent } from './read-table/read-table.component';
 import { CurrentlyReadingTableComponent } from './currently-reading-table/currently-reading-table.component';
 import { WantToReadTableComponent } from './want-to-read-table/want-to-read-table.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
 export const routes: Routes = [
   {
     path: 'register',
@@ -26,5 +29,17 @@ export const routes: Routes = [
       { path: 'currently-read', component: CurrentlyReadingTableComponent },
       { path: 'want-to-read', component: WantToReadTableComponent }
     ]
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'authors',
+    component: AuthorsComponent
   }
 ];
