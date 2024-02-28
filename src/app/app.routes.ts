d41c6954d84a05be9b9677af6695796ddd7ed65d
shimaaFathi <shimaafathi126@gmail.com>
2024-02-28 03:13:54 +0200

    edit-navbar-routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,5 @@
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { AllTableComponent } from './all-table/all-table.component';
-import { ReadTableComponent } from './read-table/read-table.component';
-import { CurrentlyReadingTableComponent } from './currently-reading-table/currently-reading-table.component';
-import { WantToReadTableComponent } from './want-to-read-table/want-to-read-table.component';
 import { CategoriesAdminComponent } from './categories-admin/categories-admin.component';
 import { BooksAdminComponent } from './books-admin/books-admin.component';
 import { AuthorsAdminComponent } from './authors-admin/authors-admin.component';
@@ -53,14 +49,7 @@ export const routes: Routes = [
   },
   {
     path: 'user',
-    component: UserComponent,
-    children: [
-      { path: '', redirectTo: 'all', pathMatch: 'full' },
-      { path: 'all', component: AllTableComponent },
-      { path: 'read', component: ReadTableComponent },
-      { path: 'currently-read', component: CurrentlyReadingTableComponent },
-      { path: 'want-to-read', component: WantToReadTableComponent },
-    ],
+    component: UserComponent
   },
 
   {
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { AllTableComponent } from "../all-table/all-table.component";
+import { ReadTableComponent } from "../read-table/read-table.component";
+import { CurrentlyReadingTableComponent } from "../currently-reading-table/currently-reading-table.component";
+import { WantToReadTableComponent } from "../want-to-read-table/want-to-read-table.component";
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [RouterModule],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+    selector: 'app-user',
+    standalone: true,
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css',
+    imports: [CommonModule, RouterModule, AllTableComponent, ReadTableComponent, CurrentlyReadingTableComponent, WantToReadTableComponent]
 })
-export class UserComponent {}
+export class UserComponent {
+    selectedTab: string = 'all'; // Default tab
+  
+    switchTab(tabName: string) {
+      this.selectedTab = tabName;
+    }
+  }
