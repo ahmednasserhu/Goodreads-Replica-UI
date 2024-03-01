0befa22e65deb75dca967086b5b6e1e485bd9b1d
ayamaher205 <aya.maher.m7md@gmail.com>
2024-03-01 16:14:31 +0200

    add avgrate of book

--- src/app/bookpage/bookpage.component.ts
+++ src/app/bookpage/bookpage.component.ts
@@ -6,6 +6,8 @@ import { RatingModule } from 'primeng/rating';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { environment } from '../../environments/environment';
+import { UserService } from '../services/user.service';
+import { catchError } from 'rxjs';
 @Component({
   selector: 'app-bookpage',
   standalone: true,
@@ -28,10 +30,10 @@ export class BookpageComponent {
     rating: number;
     bookId: String;
   };
-  shelves = ['Read', 'Want To Read', 'Currently Reading'];
-  apiUrl: String = environment.apiUrl;
+  shelves = ['read', 'currentlyReading', 'wantToRead'];
 
   constructor(
+    private userService: UserService,
     private bookRequest: BookService,
     private router: ActivatedRoute
   ) {}
@@ -45,11 +47,26 @@ export class BookpageComponent {
   }) */
 
   sendRate() {
-    const userId = window.localStorage.getItem('User');
     this.data = {
       rating: this.rate,
       bookId: this.book.id,
     };
     this.bookRequest.addRate(this.data).subscribe((res) => console.log(res));
+  }
+  addBookToShelf(event: any, bookId: String) {
+    const shelf = event.target.value;
+    if (!this.shelves.includes(shelf)) {
+      console.log('Invalid shelf name, no request sent to BackEnd');
+      return;
+    }
+    this.userService
+      .addBookToShelf(event.target.value, bookId.toString())
+      .pipe(
+        catchError((error) => {
+          console.log("Couldn't add book to shelf : ", error);
+          return error;
+        })
+      )
+      .subscribe();
   }
 }
